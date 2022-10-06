c360d992f2c4027bd941019741fd7e6a53b7d401
Ayrat Badykov <ayratin555@gmail.com>
2022-10-06 16:09:33 +0300

    fix dialyxir

--- mix.exs
+++ mix.exs
@@ -21,8 +21,8 @@ defmodule Parameter.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:dialyxir, "~> 1.0", only: [:dev, :test], runtime: false},
       {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
-      {:dialyxir, "~> 1.0", only: [:dev], runtime: false},
       {:ex_doc, "~> 0.27", only: :dev, runtime: false}
     ]
   end
