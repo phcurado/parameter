9537853f9ec45547bd63fe496ccfb90aaefeba06
phcurado <phcurado@gmail.com>
2022-10-14 15:59:17 +0300

    package info

--- mix.exs
+++ mix.exs
@@ -7,23 +7,33 @@ defmodule Parameter.MixProject do
       version: "0.1.0",
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      description: "Schema creation, validation with serialization for input data",
+      package: package(),
+      deps: deps(),
+      name: "Parameter",
+      source_url: "https://github.com/phcurado/parameter"
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
       {:dialyxir, "~> 1.0", only: [:dev, :test], runtime: false},
       {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
       {:ex_doc, "~> 0.27", only: :dev, runtime: false}
+    ]
+  end
+
+  defp package() do
+    [
+      files: ~w(lib .formatter.exs mix.exs README* LICENSE*),
+      licenses: ["Apache-2.0"],
+      links: %{"GitHub" => "https://github.com/phcurado/parameter"}
     ]
   end
 end
